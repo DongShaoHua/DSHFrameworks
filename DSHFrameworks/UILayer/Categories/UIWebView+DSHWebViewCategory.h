96ca93a4ea3e75d7e2971148a0d81f8ce1686975
dongshaohua <dongsh@dongshaohuademac.local>
2015-11-28 23:17:16 +0800

    1. 添加web view扩展，实现现实gif图片

---
+++ DSHFrameworks/UILayer/Categories/UIWebView+DSHWebViewCategory.h
@@ -0,0 +1,15 @@
+//
+//  UIWebView+DSHWebViewCategory.h
+//  DSHFrameworksIOSExample
+//
+//  Created by dongshaohua on 15/11/28.
+//  Copyright © 2015年 dongsh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (DSHWebViewCategory)
+
+- (void)loadGif:(NSString *)gifFileName;
+
+@end
---
+++ DSHFrameworks/UILayer/Categories/UIWebView+DSHWebViewCategory.m
@@ -0,0 +1,25 @@
+//
+//  UIWebView+DSHWebViewCategory.m
+//  DSHFrameworksIOSExample
+//
+//  Created by dongshaohua on 15/11/28.
+//  Copyright © 2015年 dongsh. All rights reserved.
+//
+
+#import "UIWebView+DSHWebViewCategory.h"
+
+@implementation UIWebView (DSHWebViewCategory)
+
+- (void)loadGif:(NSString *)gifFileName {
+    
+    self.backgroundColor = [UIColor clearColor];
+    self.opaque = NO;
+    self.scrollView.bounces = NO;
+    self.userInteractionEnabled = NO;
+    NSString *html = [NSString stringWithFormat: @"<!doctype html /><html><head></head><body style='margin: 0px; padding: 0px;'><img src='%@' style='width: 100%%; height: 100%%; border-width: 0px' /><body></html>", gifFileName];
+    NSString *path = [[NSBundle mainBundle] resourcePath];
+    NSURL *baseURL = [NSURL fileURLWithPath: path];
+    [self loadHTMLString: html baseURL: baseURL];
+}
+
+@end
